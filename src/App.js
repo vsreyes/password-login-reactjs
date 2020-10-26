814b8285341ef7973ea15d215423eedf2e77b3ef
vsreyes <virgilio.s.reyes@gmail.com>
2020-10-26 19:57:28 +0100

    Hide password

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import './App.css';
 
 const App = () => {
   const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
   return (
-    <div>
+    <div className='container'>
       <input
         type='text'
         placeholder='Enter username'
         value={username}
-        onChange={e => {
-          setUsername(e.target.value);
-        }}
+        onChange={e => setUsername(e.target.value)}
       />
+      <input
+        type='password'
+        placeholder='Enter password'
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
+      <h2>{username}</h2>
+      <h2>{password}</h2>
+      <button>Show/Hide password</button>
     </div>
   );
 };
